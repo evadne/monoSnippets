25a91ea56d98dc8c977e24e7463bc9b0340592a0
Evadne Wu <ev@monoceroi.com>
2010-03-15 20:19:50 +0800

    notificationCenter housekeeping fix.

--- lib.monoSnippets.notificationCenter.js
+++ lib.monoSnippets.notificationCenter.js
@@ -1,53 +1,52 @@
 //	lib.monoSnippets.notificationCenter.js
 //	Evadne Wu at Iridia Productions
-//	Version as of Mar. 13, 2010
-
+//	Version as of Mar. 15, 2010
 
 if (jQuery === undefined) return;
 if (mono === undefined) return;
 
 mono.notificationCenter = {
 
-		_notificationListeners: {},
+	_notificationListeners: {},
 
-		registerForNotificationWithKeyAndListener: function (aKey, aListenerReference) {
+	registerForNotificationWithKeyAndListener: function (aKey, aListenerReference) {
+	
+		if (mono.notificationCenter['_notificationListeners'][aKey] != undefined) {
 		
-			if (mono.notificationCenter['_notificationListeners'][aKey] != undefined) {
+			mono.info("listener reference " + aListenerReference + " for key " + aKey + " is already defined, skipping");
+		
+			return false;
+		
+		}
+
+		mono.notificationCenter._notificationListeners[aKey] = aListenerReference;
+		
+		return true;
+	
+	},
+	
+	dispatchNotificationWithKeyAndPredicate: function(aKey, aPredicate) {
+		
+		if (typeof arguments[2] == 'object')
+		if (arguments[2]['asynchronously'] === true)
+		return window.setTimeout(mono.notificationCenter.dispatchNotificationWithKeyAndPredicate(aKey, aPredicate), 0);
+		
+		$.each(mono.notificationCenter._notificationListeners, function(notificationListenerIndex, plausibleNotificationListener) {
 			
-				mono.info("listener reference " + aListenerReference + " for key " + aKey + " is already defined, skipping");
+			if (plausibleNotificationListener !== undefined) {
 			
-				return false;
+				plausibleNotificationListener(aPredicate);
 			
 			}
+			
+			mono.info("Listener " + plausibleNotificationListener + " is undefined, skipping and removing it from notification named " + aKey +  ".");
 
-			mono.notificationCenter._notificationListeners[aKey] = aListenerReference;
+			delete mono.notificationCenter._notificationListeners[notificationListenerIndex];
 			
-			return true;
+			return;
+			
+		});
 		
-		},
-		
-		dispatchNotificationWithKeyAndPredicate: function(aKey, aPredicate) {
-			
-			if (typeof arguments[2] == 'object')
-			if (arguments[2]['asynchronously'] === true)
-			return window.setTimeout(mono.notificationCenter.dispatchNotificationWithKeyAndPredicate(aKey, aPredicate), 0);
-			
-			$.each(mono.notificationCenter._notificationListeners, function(notificationListenerIndex, plausibleNotificationListener) {
-				
-				if (plausibleNotificationListener !== undefined) {
-				
-					plausibleNotificationListener(aPredicate);
-				
-				}
-				
-				mono.info("Listener " + plausibleNotificationListener + " is undefined, skipping and removing it from notification named " + aKey +  ".");
-	
-				delete mono.notificationCenter._notificationListeners[notificationListenerIndex];
-				
-				return;
-				
-			});
-			
-		}
+	}
 
 };
